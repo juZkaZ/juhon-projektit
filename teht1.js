435e27794d1179b33960ec03d0886cbd5cfe3c83
Juho Murtoniemi <juho.murtoniemi@gmail.com>
2020-10-26 15:33:59 +0200

    week 1 teht1

---
+++ teht1.js
@@ -0,0 +1,12 @@
+const numb1 = 1;
+const numb2 = 5;
+
+const sum = numb1 + numb2;
+const fraction = numb1 - numb2;
+const product = numb1 * numb2;
+const average = numb1 + numb2 / 2;
+
+console.log ("The sum of numbers is " + sum);
+console.log ("The sum of numbers is " + fraction);
+console.log ("The sum of numbers is " + product);
+console.log ("The sum of numbers is " + average);
